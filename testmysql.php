73d4b0ce40b4e22d15fa4211a86a18cead77847c
Nicolas Escobar <nicoescobar69@gmail.com>
2016-09-13 17:54:47 -0500

    Se agrego contraseña

--- testmysql.php
+++ testmysql.php
@@ -4,7 +4,7 @@
 * Change the value of parameter 3 if you have set a password on the root userid
 * Changer la valeur du 3e paramètre si vous avez mis un mot de passe à root
 */
-$mysqli = new mysqli('127.0.0.1', 'root', '');
+$mysqli = new mysqli('127.0.0.1', 'root', 'legolas4129');
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
