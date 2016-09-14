a927b5ac5ae514c272e6ad1b0b272bd51ead86de
Nicolas Escobar <nicoescobar69@gmail.com>
2016-09-13 19:11:16 -0500

    correccion sintaxys

--- consumo_agua.php
+++ consumo_agua.php
@@ -17,19 +17,13 @@ if ($conn->connect_error) {
 $litros = $_GET['litros'];
 $precio = $_GET['precio'];
 
-//$sql = "INSERT INTO lecturas (valor, id)
-//VALUES ('510', '03')";
-//$sql = "INSERT INTO lecturas (valor)
-//VALUES (".$val.")";
 
 //$sql = "INSERT INTO lecturas (valor, id, 	nombre)
 //VALUES (".$val.",".$id.",'".$nombre."')";
 
 $sql = "INSERT INTO consumo_agua (litros, precio)
-VALUES (".$litros.",".$precio.")";
+VALUES (".$litros.",'".$precio."')"; // Al final de cada sentencia debe ir '".$variable."'
 
-//mysqli_query($con,"INSERT INTO Persons (FirstName,LastName,Age) 
-//VALUES ('Glenn','Quagmire',33)");
 
 if ($conn->query($sql) === TRUE) {
     echo "Datos insertados Correctamente";
